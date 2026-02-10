7f855e0c0933475120c3ded35dbcc26222430d32
Reyner Paul Bering <53499535+reynerbering@users.noreply.github.com>
2026-02-11 03:58:39 +0800

    some and every

--- starter/script.js
+++ starter/script.js
@@ -172,6 +172,20 @@ btnTransfer.addEventListener('click', function (e) {
   }
 });
 
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const amount = Number(inputLoanAmount.value);
+
+  if(amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+    currentAccount.movements.push(amount);
+    updateUI(currentAccount);
+    inputLoanAmount.textContent = '';
+  }
+
+});
+
+
 btnClose.addEventListener('click', function (e) {
   e.preventDefault();
 
@@ -249,7 +263,21 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 // const account= accounts.find(acc => acc.owner === "Jessica Davis");
 // console.log(account);
 
-const lastWithdrawal = movements.findLast(mov => mov < 0);
-const lastLargeMovement = movements.findLastIndex(mov => Math.abs(mov)> 1000);
+// const lastWithdrawal = movements.findLast(mov => mov < 0);
+// const lastLargeMovement = movements.findLastIndex(mov => Math.abs(mov)> 1000);
 
-console.log (`Your latest large movement was ${movements.length - lastLargeMovement } movements ago`);
+// console.log (`Your latest large movement was ${movements.length - lastLargeMovement } movements ago`);
+
+console.log(movements);
+console.log(movements.includes(-130));
+
+const anyDeposits = movements.some(mov => mov > 0);
+console.log(anyDeposits);
+
+console.log(movements.every(mov => mov > 0));
+console.log(account4.movements.every(mov => mov > 0));
+
+const deposit = mov => mov > 0;
+console.log(movements.some(deposit));
+console.log(movements.every(deposit));
+console.log(movements.filter(deposit));
